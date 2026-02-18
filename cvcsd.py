26ea4c416b695320bea4c675a00b2ed7404c4e52
cheikhsadiboudiakhame-netizen <cheikhsadiboudiakhame@gmail.com>
2026-02-18 12:53:51 +0100

    Add files via upload

---
+++ cvcsd.py
@@ -0,0 +1,53 @@
+import streamlit as st
+st.title("Mon proforiot")
+sidebar=st.sidebar
+with sidebar:
+    st.image("badboycsd.jpeg")
+    st.write("Prenom: Cheikh Sadibou")
+    st.write("Nom: DIAKHAME")
+    st.write("Adresse:Parcelles Assainies Dakar unité 14")
+    st.write("Téléphone:784527100")
+    st.write("Contact:cheikhsadiboudiakhame@gmail.com")
+
+########################################
+
+st.header("A PROPOS ")
+st.write("Géomaticien et Géographe de la santé,specialisé dans l'analyse spatiale des données sanitaires et la cartographie thématique. Je met les outils SIG au service de l'amélioration des soins, de l'analyse des inégalités territoriales de santé et de l'aide á la prise de decision.")
+
+#######################################
+
+st.header("PROJET")
+st.markdown("""SIG appliqué á la santé publique
+    Objectif:utiliser les SIG pour appuyer la prise de decision sanitaires.
+    Outils:QGIS,ARCGIS,base de données sanitaires.
+    Resultat:carte d'aide a la décision pour les acteurs de santé.""")
+
+#######################################
+
+st.header("DIPLOMES")
+st.write("Brevet Technicien Supérieur en géomatique")
+st.write("Licence en geographie")
+st.write("Baccalauréat")
+
+#########################################
+
+st.header("COMPETENCES")
+st.write("Cartographie Thematique(santé, environnement,population")
+st.write("Analyse Spatiaux-Temporelle des Maladies")
+st.write("Creation et Gestion de Base de Données Geographiques dans QGIS et ARCGIS")
+st.write("Georeferencement et Digitalisation dans QGIS et ARCGIS")
+st.write("Production de Carte")
+st.write("Collecte et Traitement de Donnees")
+st.write("Bureautique(word,excel,power point)")
+
+###########################################
+
+st.header("EXPERIENCE")
+st.write(" Agence National de la la Statistiquue et de la Démographie, j ai eu a travailler comme agent controleur lors du RGPH-5")
+
+###########################################
+
+st.header("CENTRE D'INTERET")
+st.write("Lire")
+st.write("Footbal")
+st.write("Art Martiaux")
